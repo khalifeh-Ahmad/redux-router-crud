460810b7828399563351671c746bac1d085a8161
khalifeh Ahmad <khalifeh92ahmed@hotmail.com>
2023-10-16 11:49:18 -0800

    routing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RoutLayout from "./pages/RoutLayout";
+import AddPost from "./pages/AddPost";
+import EditPost from "./pages/EditPost";
+import Details from "./pages/Details";
 
-import App from "./App";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RoutLayout />,
+    children: [
+      { path: "add", element: <AddPost /> },
+      { path: ":id/edit", element: <EditPost /> },
+      { path: ":id", element: <Details /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/pages/AddPost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddPost = () => {
+  return (
+    <div>addPOSTTTTTTTTTTTT</div>
+  )
+}
+
+export default AddPost
