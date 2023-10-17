427e42aedd605919d5da3197decbddcb94ec69a7
khalifeh Ahmad <khalifeh92ahmed@hotmail.com>
2023-10-16 17:36:08 -0800

    finish Routing

--- src/index.js
+++ src/index.js
@@ -7,15 +7,31 @@ import RoutLayout from "./pages/RoutLayout";
 import AddPost from "./pages/AddPost";
 import EditPost from "./pages/EditPost";
 import Details from "./pages/Details";
+import Index from "./pages/Index";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RoutLayout />,
+    errorElement: <ErrorPage />,
     children: [
+      { index: true, element: <Index /> },
+      { path: "post", element: <Index /> },
       { path: "add", element: <AddPost /> },
       { path: ":id/edit", element: <EditPost /> },
-      { path: ":id", element: <Details /> },
+      {
+        path: ":id",
+        element: <Details />,
+        loader: (data) => {
+          if (isNaN(data.params.id)) {
+            throw new Response("Bad Request", {
+              statusText: "please insert an ID",
+              status: 400,
+            });
+          }
+        },
+      },
     ],
   },
 ]);
